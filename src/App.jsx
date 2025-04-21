a8efe58d78f29cb82f8da6570d24370e29bcfb29
Cleve-keem <belloharyourmidey@gmail.com>
2025-04-21 09:02:00 -0500

    built the router and Applayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../src/ui/Home";
+import Menu from "./features/menu/Menu";
+import AppLayout from "./ui/AppLayout";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
